c86b598294f6a774a57aab25a6d0dc60726f978f
Ivan Ilyin <114531025+evfr-pol@users.noreply.github.com>
2026-04-07 20:26:28 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,42 @@
+from model import Prediction
+
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+@st.cache_resource
+def load_model():
+    model_preds = Prediction(768, 8, 0.2, './base_model.pt')
+    return model_preds
+
+
+model = load_model()
+st.title("Article Themes by Title and Abstract")
+
+title = st.text_input("Title", placeholder="Write article's title...")
+
+abstract = st.text_area(
+    "Abstract", 
+    height=250,
+    placeholder="Write article's abstract..."
 )
+
+label_full_names = {
+    'cs': 'Computer Science',
+    'econ': 'Economics',
+    'eess': 'Electrical Engineering',
+    'math': 'Mathematics',
+    'q-bio': 'Quantitative Biology',
+    'q-fin': 'Quantitative Finance',
+    'stat': 'Statistics',
+    'phys': 'Physics',
+}
+
+if st.button("Predict"):
+    if title:
+        labels = model.get_labels(title, abstract)
+        if labels:
+            result_text = ', '.join([label_full_names.get(label, label) for label in labels])
+            st.success(result_text)
+        else:
+            st.warning("No labels predicted.")
+    else:
+        st.error("Please, fill title fields")
